f1de811441159f51841eddb38efa33f2c22519b1
John Heasly <jheasly@earthlink.net>
2019-01-04 15:29:48 -0800

    basic bits working.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Tabletop from 'tabletop';
 
 /*
 https://medium.com/@ryan.mcnierney/using-react-google-sheets-as-your-cms-294c02561d59
@@ -10,22 +11,29 @@ https://blog.416serg.me/building-an-app-using-google-sheets-api-react-d69681d22c
 
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      data: []
+    }
+  }
+
+  componentDidMount() {
+    Tabletop.init({
+      key: '1kOHYujR27_k1KTtXmpgNvvhdPohyQ_RefjN1tCrqONA',
+      callback: googleData => {
+        console.log('google sheet data --->', googleData)
+      },
+      simpleSheet: true
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1 className="App-title">React + Google Sheets demo</h1>
         </header>
       </div>
     );
